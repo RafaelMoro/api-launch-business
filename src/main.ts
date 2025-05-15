2b621129c36012edde1582866f8be4fb2096e331
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 15:57:36 -0600

    add filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GeneralAppExceptionFilter } from './exceptions/GeneralExceptionFilter.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GeneralAppExceptionFilter());
   await app.listen(process.env.PORT ?? 6060);
 }
 bootstrap();
