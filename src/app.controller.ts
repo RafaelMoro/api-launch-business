aa7a8a8082fd04f6c1c35faf1c5f3b53c92798b2
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 17:31:29 -0600

    add endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { BusinessPlanDto } from './app.dto';
 
 @Controller()
 export class AppController {
@@ -8,6 +9,11 @@ export class AppController {
   @Get()
   testApi() {
     return this.appService.testApi();
+  }
+
+  @Post('/business-plan')
+  generateBusinessPlan(@Body() data: BusinessPlanDto) {
+    return this.appService.generateBusinessPlan(data);
   }
 
   @Get('/chat')
