4c20622684041dc990953df1075386d41636aeb1
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 16:06:40 -0600

    add testApi service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import OpenAI from 'openai';
 import config from '@/config';
+
+import { VERSION_RESPONSE } from '@/constants';
+import { TestApiResponse } from '@/interface';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
+
+  testApi() {
+    const response: TestApiResponse = {
+      version: VERSION_RESPONSE,
+      success: true,
+      message: 'ok',
+      data: null,
+      error: null,
+    };
+    return response;
+  }
+
   async getHello() {
     try {
       const apiKey = this.configService.openAIKey ?? '';
@@ -19,20 +36,26 @@ export class AppService {
         apiKey,
       });
       // Completion for web search
-      const completion = await openai.responses.create({
-        model: 'gpt-4.1',
-        tools: [{ type: 'web_search_preview' }],
-        input: 'What was a positive news story from today?',
-      });
-      const result = completion.output_text;
+      // const completion = await openai.responses.create({
+      //   model: 'gpt-4.1',
+      //   tools: [{ type: 'web_search_preview' }],
+      //   input: 'What was a positive news story from today?',
+      // });
+      // const result = completion.output_text;
 
       // Completion for chat
-      // const completion = await openai.chat.completions.create({
-      //   model: 'gpt-4o-mini',
-      //   store: true,
-      //   messages: [{ role: 'user', content: 'write a haiku about ai' }],
-      // });
-      // const result = completion.choices[0].message;
+      const completion = await openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        store: true,
+        messages: [
+          {
+            role: 'user',
+            content:
+              'based on this longitude 19.101495454823958 and latitude -96.12098351131316, determine what is the state and country of the location',
+          },
+        ],
+      });
+      const result = completion.choices[0].message;
 
       return {
         test: result,
