4f608a44a9abb7ab2ee4c6c621d91df683cc3ea7
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 08:29:50 -0600

    test open ai

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, BadRequestException } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import OpenAI from 'openai';
+import config from '@/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+  async getHello() {
+    try {
+      const apiKey = this.configService.openAIKey ?? '';
+      if (!apiKey) {
+        throw new BadRequestException('API key is not set');
+      }
+
+      const openai = new OpenAI({
+        apiKey,
+      });
+      const completion = await openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        store: true,
+        messages: [{ role: 'user', content: 'write a haiku about ai' }],
+      });
+      const result = completion.choices[0].message;
+
+      return {
+        test: result,
+        satus: 'ok',
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
