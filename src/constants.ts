421dd0b0eb295481cec648754cb409c3c49caad2
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 19:34:24 -0600

    add function constant of prompt

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,14 @@
+import {
+  GetBasePromptBusinessPlanProps,
+  GetBuyerPersonaProps,
+} from './interface';
+
 export const VERSION_RESPONSE = process.env.npm_package_version ?? '0.0.0';
 
 // Prompts
 export const getBasePromptBusinessPlan = ({
   business,
-}: {
-  business: string;
-}) => `
+}: GetBasePromptBusinessPlanProps) => `
   Define un plan de negocio para ${business}. La estrategia de negocio debe incluir: 
   1. Una estrategia de negocio 
   2. Define un plan para atraer inversion
@@ -20,5 +23,25 @@ export const getBasePromptBusinessPlan = ({
     "possibleRisks": ""
   }
 
+  Recuerda no devolver en el resultado ningun salto de linea, ni /n, ni comillas, ni caracteres especiales.
+`;
+
+export const getBuyerPersona = ({
+  business,
+  state,
+  country,
+}: GetBuyerPersonaProps) => `
+  Eres un experto en marketing digital.
+  Define 3 buyer persona para el negocio de ${business} en base a este estado ${state} y pais ${country}.
+  Devuelve el resultado en un arreglo de JSON con este formato:
+
+  {
+  "name": "",
+  "demographicData": "",
+  "behaviorMotivations": "",
+  "needs": "",
+}
+
+  En caso de que ${state} y ${country} sean invalidos, usa al país de México como referencia.
   Recuerda no devolver en el resultado ningun salto de linea, ni /n, ni comillas, ni caracteres especiales.
 `;
